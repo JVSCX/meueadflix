a1085f891e9bee83cc3378dcabf983156567199c
JVSCX <joaovittorsp@hotmail.com.br>
2023-02-21 20:23:27 -0300

    teste

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'meueadflix_development',
+    username: 'meueadflix',
+    password: 'meueadflix'
+  }
+}
