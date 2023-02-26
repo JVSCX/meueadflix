501eba0461713c7fcbfe2a8c70e4adee3d40879c
JVSCX <joaovittorsp@hotmail.com.br>
2023-02-26 13:42:47 -0300

    rota

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { adminjs, adminJsRouter } from "./adminjs";
 import { sequelize } from "./database/index.";
+import { router } from "./routes";
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(adminjs.options.rootPath, adminJsRouter)
+app.use(adminjs.options.rootPath, adminJsRouter);
+
+app.use(router);
 
 const PORT = process.env.port || 3000;
 
