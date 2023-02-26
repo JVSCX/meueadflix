b18c790bc4358e8c33513c1cbf0c755a6ce6e5dc
JVSCX <joaovittorsp@hotmail.com.br>
2023-02-26 16:39:37 -0300

    obtendo lançamentos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -7,7 +7,18 @@ export const coursesController = {
     try {
       const featuredCourses = await courseService.getRandomFeaturedCourses()
       return res.json(featuredCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
 
+  //GET /courses/newest
+  newest: async (req: Request, res: Response) => {
+    try {
+      const newestCourses = await courseService.getTopTenNewest()
+      return res.json(newestCourses)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
