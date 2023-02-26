5e8a60a975d4e18f048034b30997e73bb10e7203
JVSCX <joaovittorsp@hotmail.com.br>
2023-02-26 18:34:56 -0300

    registro de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { authController } from "./controllers/authController";
 import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 
 const router = express.Router();
+
+router.post("/auth/register", authController.register);
 
 router.get("/categories", categoriesController.index);
 router.get("/categories/:id", categoriesController.show);
@@ -13,6 +16,6 @@ router.get("/courses/newest", coursesController.newest);
 router.get("/courses/search", coursesController.search);
 router.get("/courses/:id", coursesController.show);
 
-router.get('/episodes/stream', episodesController.stream)
+router.get("/episodes/stream", episodesController.stream);
 
 export { router };
