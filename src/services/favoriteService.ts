eeb3d0304ed5d2bc28bb40bf392e0fb8d2c098f4
JVSCX <joaovittorsp@hotmail.com.br>
2023-03-05 16:13:27 -0300

    favoritos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,6 +1,27 @@
 import { Favorite } from "../models";
 
 export const favoriteService = {
+  findByUserId: async (userId: number) => {
+    const favorite = await Favorite.findAll({
+      attributes: [["user_id", "userId"]],
+      where: { userId: userId },
+      include: {
+        association: "Course",
+        attributes: [
+          "id",
+          "name",
+          "synopsis",
+          ["thumbnail_url", "thumbnailUrl"],
+        ],
+      },
+    });
+
+    return {
+      userId,
+      courses: favorite.map(favorite => favorite.courseId)
+    }
+  },
+
   create: async (userId: number, courseId: number) => {
     const favorite = await Favorite.create({
       userId,
