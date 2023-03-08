cfa8230cde67c072e6f8188f4a4d1e02f563b9a5
JVSCX <joaovittorsp@hotmail.com.br>
2023-03-08 19:59:49 -0300

    add o CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { adminjs, adminJsRouter } from "./adminjs";
 import { sequelize } from "./database/index.";
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static("public"));
 
